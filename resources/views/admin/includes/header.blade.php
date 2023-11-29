82dbadc5d5f6d46631842e8d22cde3f631570806
PleaseHelpNorvin <norvincrujido90@gmailcom>
2023-11-29 11:49:56 +0800

    nvm back to the default

--- resources/views/admin/includes/header.blade.php
+++ resources/views/admin/includes/header.blade.php
@@ -36,21 +36,6 @@
 
 {{-- CALENDAR -02 --}}
 
-<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-{{-- <link rel="stylesheet" href="css/style.css"> --}}
-<link rel="stylesheet" href="{!! asset('assets/calendar-02/calendar-02/css/style.css')!!}">
-
-<script src="{!! asset('assets/calendar-02/calendar-02/js/jquery.min.js') !!}"></script>
-<script src="{!! asset('assets/calendar-02/calendar-02/js/popper.js') !!}"></script>
-<script src="{!! asset('assets/calendar-02/calendar-02/js/bootstrap.min.js') !!}"></script>
-<script src="{!! asset('assets/calendar-02/calendar-02/js/main.js') !!}"></script>
-
-
-    {{-- <script src="js/jquery.min.js"></script> --}}
-    {{-- <script src="js/popper.js"></script> --}}
-    {{-- <script src="js/bootstrap.min.js"></script> --}}
-    {{-- <script src="js/main.js"></script> --}}
 
 
 {{-- faculty list/subject list library --}}
